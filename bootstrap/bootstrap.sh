834c00789ee31d3c40f570e421fb86a588a3508c
dom38 <dominicbyrne38@hotmail.com>
2023-06-17 11:07:49 +0100

    Add hostpath to bootstrap

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -4,3 +4,4 @@ kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl apply -f argocd-resources/templates/core-project.yaml -n argocd
 kubectl apply -f argocd-resources/templates/core-application.yaml -n argocd
+kubectl apply -f argocd-resources/templates/hostpath.yaml
